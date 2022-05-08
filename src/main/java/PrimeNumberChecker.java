533b2fa98b037abe3aa6ea90480a028c240b8938
mcelejewski <mcelejewski@gispartner.pl>
2022-05-08 17:27:13 +0200

    Homework corrected

--- src/main/java/PrimeNumberChecker.java
+++ src/main/java/PrimeNumberChecker.java
@@ -6,7 +6,12 @@ public class PrimeNumberChecker {
     public static void main(String[] args) {
         boolean conRun = true;
         while(conRun) {
-            isPrime(readGivenNumber());
+
+            if(isPrime(readGivenNumber()) == true){
+                System.out.println("The number You entered is not a prime number.\n\n");
+            }else{
+                System.out.println("The number You entered is a prime number\n\n");
+            }
         }
     }
 
@@ -16,35 +21,30 @@ public class PrimeNumberChecker {
         System.out.println("ENTER 0 OR 1 IF YOU WANT TO EXIT PROGRAM");
         System.out.println("Enter only one number to check if its prime: ");
             try {
-                number = scanner.nextInt();
-
-                if (number == 0 || number ==1) {
-                    System.exit(0);
-                } else if (number < 0) {
-                    System.out.println("Wrong input! You can enter only positive numbers \n");
-                }
-//                if(scanner.hasNextInt())
-//                {
-//                    System.out.println("Enter one value! Only first value will be checked!");
-//                }
+                do {
+                    number = scanner.nextInt();
+                    if (number == 0 || number == 1) {
+                        System.exit(0);
+                    } else if (number < 0) {
+                        System.out.println("Wrong input! You can enter only positive numbers!");
+                        System.out.println("Choose another number: ");
+                    }
+                }while(number<0);
             } catch (InputMismatchException e) {
                 System.out.println("Wrong input! You can enter only positive numbers! \n");
             }
         return number;
     }
 
-    public static void isPrime(int num){
+    public static boolean isPrime(int num){
         double border = Math.round(Math.sqrt(num));
-        boolean ifNotPrimeFlag = false;                      //Flaga ustawiana, aby przy liczbie niepierwszej, nie
-        for(int i =2; i<=border; i++){                       //był wyświetlany komunikat o tym że liczba jest pierwsza
+        boolean ifNotPrimeFlag = false;
+        for(int i =2; i<=border; i++){
             if(num%i ==0){
-                System.out.println("The number You entered is not a prime number. It can be divide by: " + i + "\n\n");
                 ifNotPrimeFlag = true;
                 break;
             }
-        }if(ifNotPrimeFlag == false && num >0) {
-            System.out.println("The number You entered is a prime number\n\n");
         }
+        return ifNotPrimeFlag;
     }
-
 }
